081c5c3247c37f1eac963529c1fa9e6d3d2eb053
Mark <72468363+dievs-vana@users.noreply.github.com>
2022-03-11 01:27:57 +0800

    new API and New templates
    new template patient.php for fill up form of adding patient

--- api/patient/addPatient.php
+++ api/patient/addPatient.php
@@ -2,9 +2,23 @@
     require '../../config/dbConfig.php';
 
     $student_id = $_POST['student_id'];
-    $status = $_POST['status'];
-    
-    $sql = "INSERT INTO brand (student_id, status) VALUES ('".$student_id."')";
+    $course = $_POST['course'];
+    $school_year = $_POST['school_year'];
+    $full_name = $_POST['full_name'];
+    $birth_date = $_POST['birth_date'];
+    $addRess = $_POST['addRess'];
+    $mobileno = $_POST['mobileno'];
+    $year_old = $_POST['year_old'];
+    $gender = $_POST['gender'];
+    $marital_status = $_POST['marital_status'];
+    $nationality = $_POST['nationality'];
+    $religion = $_POST['religion'];
+    $emergency_guardian = $_POST['emergency_guardian'];
+    $relation_ship = $_POST['relation_ship'];
+    $emergency_contact = $_POST['emergency_contact'];
+
+
+    $sql = "INSERT INTO patient (student_id, course, school_year, full_name, birth_date, addRess, mobile_no, year_old, gender, marital_status, nationality, religion, emergency_guardian, relation_ship, emergency_contact) VALUES ('".$student_id."', '".$course."', '".$school_year."', '".$full_name."', '".$birth_date."', '".$addRess."', '".$mobileno."', '".$year_old."', '".$gender."', '".$marital_status."', '".$nationality."', '".$religion."', '".$emergency_guardian."', '".$relation_ship."', '".$emergency_contact."')";
     if (mysqli_query($conn, $sql)) {
         echo "New record created successfully";
     } else {
